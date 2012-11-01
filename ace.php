b8c8ee5826b38ebf94d8b19b55d2d328f25b8f9d
svenbluege <svenbluege@315069c3-c3a2-4022-846d-82834a413b37>
2012-11-01 16:35:59 +0000

    fixed zindex with new Joomla 3 header and footer in full screen mode
    git-svn-id: http://joomlacode.org/svn/plg_ace_editor/trunk@6 315069c3-c3a2-4022-846d-82834a413b37

--- ace.php
+++ ace.php
@@ -55,7 +55,7 @@ class plgEditorAce extends JPlugin
 			        bottom: 0;
 			        left: 0;
 			        right: 0;
-			        z-index: 100;
+			        z-index: 2000;
 			        background: white;
 			    }
 
